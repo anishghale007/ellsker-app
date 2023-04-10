15734b1fc15c1d2efad8e34670d4f6fa3d617082
anishghale007 <anishghalegg7@gmail.com>
2023-04-10 10:55:45 +0545

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:internship_practice/colors_utils.dart';
 import 'package:internship_practice/router.dart';
 import 'package:internship_practice/ui_pages.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
